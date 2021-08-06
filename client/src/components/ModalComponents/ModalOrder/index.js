b8db41111f7c20e9a314f37dccc44612f4e9d79f
Sheinichi <sheinamaeguardian@gmail.com>
2021-08-06 11:07:18 +0800

    Last day. Ayawan naaaa

--- client/src/components/ModalComponents/ModalOrder/index.js
+++ client/src/components/ModalComponents/ModalOrder/index.js
@@ -1,87 +1,79 @@
+import Modal from "../../../components/ModalComponents/ModalCheckout/index";
+import Backdrop from "../../../components/ModalComponents/Backdrop";
+import { useState } from "react";
 function ModalOrder({name,imageURL,price,...props}){
+    const [modalIsOpen, setModalIsOpen] = useState(false); 
+  
     function cancelHandler(){
         props.onCancel();
     }
     function checkoutHandler(){
-        props.onCancel();
+        props.onConfirm();
     }
+ 
+
 
     return(
 
          
-    <div className='modal'>
-<center>
-    <p>ORDER</p>    
-    <hr/>
-            <h3>{name}</h3>
-            <br />
-            <img src={imageURL}/><br/>
-           
-            <p>{ price}</p>
-    <br/><br/>
-    Quantity: &nbsp; <input type="number" placeholder="Enter Quantity"/>
-    <br/> <br/>
-    {/* Mode of Payment:<br/>
-    1 <input type="radio" name="payment1"/><br/>
-    2 <input type="radio" name="payment1"/>
-    <br/> <br/> <br/> <br/>
-     */}
-    <button className='order-btn-btn' onClick={cancelHandler}> Cancel</button> 
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <button className='order-btn-btn' onClick={checkoutHandler}>Checkout</button>
-    </center>
-</div>
-    
-    // <div className='order'>
-    //      <div>
-    //         {/* display item's picture here */}
-    //         {/* display item's picture here */} 
+    <div className='modal-order'>
+        <center>
+        <p>ORDER</p>    
+        <hr/>
+                <h3>{name}</h3>
+                <br />
+                <img src={imageURL} height="100px" width="130"/><br/>
             
-    //         Quantity: <input type="text" placeholder="Enter Quantity"/><br/>
-    //         Mode of payment: <input type="radio" placeholder="Payment1">Payment1</input>
-    //         <input type="radio" placeholder="Payment2">Payment2</input>
-               
+                <p>{ price}</p>
+        <br/><br/>
+        Quantity: &nbsp; <input type="number" placeholder="Enter Quantity"/>
+        <br/> <br/>
+   
+        {/* <button className='order-btn-btn' onClick={cancelHandler}> Cancel</button>  */}
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </center>
 
+        <br/> <br/><br/> <br/><br/> <br/>
+        <Checkout/>
+        
 
-    //     </div>
-    //     <button className='btn btn--alt' onClick={cancelHandler}> Cancel</button>
-    //     <button className='btn' onClick={confirmHandler}>Confirm</button>
-    // </div>
+    </div>
     )
 }
 
-// function Checkout(props) {
-// 	const [modalIsOpen, setModalIsOpen] = useState(false); //modal should not be opened first so false
+function Checkout(props) {
+	const [modalIsOpen, setModalIsOpen] = useState(false); //modal should not be opened first so false
 
-// 	function deleteHandler() {
-// 		setModalIsOpen(true);
-// 		// console.log('Clicked!');
-// 		// console.log(props.text);
-// 	}
-// 	function closeModalHandler() {
-// 		setModalIsOpen(false);
-// 	}
+	function deleteHandler() {
+		setModalIsOpen(true);
+	}
+	function closeModalHandler() {
+		setModalIsOpen(false);
+	}
 
-// 	return (
-// 		<div className="order-btn">
-// 			<h2>{props.text}</h2>
-// 			<div className="order-actions">
-// 				<button className="order-btn-btn" onClick={deleteHandler}>
-// 				<span class="material-icons">info</span>
-// 				</button>
-// 			</div>
-// 			{modalIsOpen && (
-// 				<Modal {...props} onCancel={closeModalHandler} onConfirm={closeModalHandler} />
-// 			)}
-// 			{modalIsOpen && <Backdrop onClick={closeModalHandler} />}
-// 			{/* shortcut, than to use the one below */}
-// 			{/* {modalIsOpen ? <Modal/> : null}
-// 			 **if modalIsOpen is true, we render Modal,else, we dont output anything (null) */}
-// 		</div>
-// 	);
-// }
+
+    return (
+		<div className="order-btn">
+			<h2>{props.text}</h2>
+			<div className="order-actions">
+			Checkout &nbsp;
+				<button className="order-btn-btn" onClick={deleteHandler}>
+				<span class="material-icons">add_shopping_cart</span>
+				</button>
+			</div>
+			{modalIsOpen && (
+				<Modal {...props} onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+			)}
+			{modalIsOpen && <Backdrop onClick={closeModalHandler} />}
+			{/* shortcut, than to use the one below */}
+			{/* {modalIsOpen ? <Modal/> : null}
+			 **if modalIsOpen is true, we render Modal,else, we dont output anything (null) */}
+		</div>
+	);
+
+}
 
 export default ModalOrder;
--- client/src/pages/Seller/Inventory/AddProductForm/bl.js
+++ client/src/pages/Seller/Inventory/AddProductForm/bl.js
@@ -3,58 +3,64 @@ import { useFormik } from "formik";
 import ValidationSchema from "./validition";
 import { CREATE_PRODUCT } from "./graphql";
 import { UploadImage } from "../../../../adapters/S3Adapter";
-export default function AddProductFormBL(selectedProduct) {
-	const [createProduct, { data, loading, error }] =
-		useMutation(CREATE_PRODUCT);
+export default function AddProductFormBL() {
+  const [createProduct, { data, loading, error }] = useMutation(CREATE_PRODUCT);
 
-	const handleSubmit = async (values, { setSubmitting }) => {
-		// upload then execute mutation
-		console.log(values);
-		var imageURL = null;
-		await UploadImage(values.image).then((res) => {
-			imageURL = res.url;
-			console.log(imageURL);
-		});
+  const handleSubmit = async (values, { setSubmitting }) => {
+    // upload then execute mutation
+    console.log(values);
+    var imageURL = null;
+    await UploadImage(values.image).then((res) => {
+      imageURL = res.url;
+      console.log(imageURL);
+    });
 
-		if (imageURL) {
-		console.log(values);
-			try {
-				createProduct({
-					variables: {
-						product: {
-							description: values.description,
-							name: values.productName,
-							price: values.price,
-							quantity: values.quantity,
-							imageURL: imageURL,
-						},
-					},
-				});			
-			} catch (err) {
-			console.log(err)	
-		}
-		}
-		setSubmitting(false);
-	};
-	const initialValues = (product) => {
-		console.log(product)
-		if (!product) {
-			return {
-				id: null,
-				productName: "",
-				description: "",
-				price: 0,
-				quantity:0,
-				image: null,			
-			}			
-		}
-		return product
-	}
-	const formik = useFormik({
-		initialValues: initialValues(selectedProduct),
-		validationSchema: ValidationSchema,
-		onSubmit: handleSubmit,
-	});
+    if (imageURL) {
+      console.log(values);
+      try {
+        createProduct({
+          variables: {
+            product: {
+              description: values.description,
+              name: values.productName,
+              price: values.price,
+              quantity: values.quantity,
+              imageURL: imageURL,
+            },
+          },
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    setSubmitting(false);
+  };
+//   const initialValues = (product) => {
+//     console.log(product);
+//     if (!product) {
+//       return {
+//         id: null,
+//         productName: "",
+//         description: "",
+//         price: 0,
+//         quantity: 0,
+//         image: null,
+//       };
+//     }
+//     return product;
+//   };
+  const formik = useFormik({
+    initialValues:  {
+        id: "",
+        productName: "",
+        description: "",
+        price: 0,
+        quantity: 0,
+        image: "",
+      },
+    validationSchema: ValidationSchema,
+    onSubmit: handleSubmit,
+  });
 
-	return { formik };
+  return { formik };
 }
